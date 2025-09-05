a4fe1323703cf3a237343387caa5391b01366f19
Allan Gabriel da Silva <allangabrielsilva0602@gmail.com>
2025-09-05 10:26:45 -0300

    Adicionando servidor

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = [
+  {
+    id: 'j43b5jh4b5h345b',
+    name: 'Allan',
+    age: 18,
+    email: 'allan@gmail.com'
+  },
+  {
+    id: 'j43b45nkbk345h345b',
+    name: 'Helo',
+    age: 18,
+    email: 'helo@gmail.com'
+  }
+];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const { name, age, email } = req.body;
+
+  if (!name || !age || !email) {
+    return res.status(400).json({ error: "Preencha todos os campos" });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    age,
+    email
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  users = users.filter(user => user.id !== id);
+  res.json({ message: "Usuário removido com sucesso!" });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
